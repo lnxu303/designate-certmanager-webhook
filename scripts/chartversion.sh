0bb81df0865177b45fb7c3942300bb62db9c030c
Fred78290 <frederic.boltz@gmail.com>
2024-03-30 11:58:12 +0100

    WIP: Helm chart publication

--- scripts/chartversion.sh
+++ scripts/chartversion.sh
@@ -2,9 +2,21 @@
 TAG=${GITHUB_REF#refs/tags/v}
 
 cat > helm/designate-certmanager-webhook/Chart.yaml <<EOF
-apiVersion: v1
+apiVersion: v2
 appVersion: "1.0.0"
-description: ACME webhook Implementation for OpenStack Designate
+description: A Helm chart to install designate-certmanager-webhook
 name: designate-certmanager-webhook
 version: "${TAG}"
+home: https://github.com/Fred78290/designate-certmanager-webhook/
+icon: https://raw.githubusercontent.com/Fred78290/designate-certmanager-webhook/master/images/cert-manager-webhook.png
+keywords:
+  - cert-manager
+  - designate
+  - openstack
+  - dns-saas
+maintainers:
+  - name: Fred78290
+    url: https://github.com/Fred78290/
+sources:
+  - https://github.com/Fred78290/designate-certmanager-webhook/
 EOF
